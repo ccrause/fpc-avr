af1e48132a328cddc0dd0206b1727de487a43ad0
ccrause <christo.crause@gmail.com>
2018-06-26 21:57:51 +0200

    Added code to support ATtiny45

--- src/examples/blink2/blink.pp
+++ src/examples/blink2/blink.pp
@@ -1,31 +1,53 @@
 program blink;
 
+uses
+  intrinsics;
+
 const
-  PB5 = 1 shl 5;
+  LEDpin = 1 shl 4;
+  CS00msk = 1 shl 0;
+  CS01msk = 1 shl 1;
+  CS02msk = 1 shl 2;
+
+  TOIE0msk =
+  {$ifdef FPC_MCU_ATTINY45}
+   1 shl 1;
+  {$else} // defaults to atmega328p
+   1 shl 0;
+  {$endif}
 
 var
-  pinport: byte absolute PORTB;
-  pindir: byte absolute DDRB;
+  countsPerSecond: byte;
+  LEDport: byte absolute PORTB;
+  LEDdir: byte absolute DDRB;
   i: byte = 1;
 
 procedure Timer0Overflow; alias: 'TIMER0_OVF_ISR'; interrupt;
 begin
   inc(i);
-  if i > 61 then  // Interrupt occurs @ 16000000/1024/256 = 61.035...Hz
+  if i > countsPerSecond then
   begin
-    pinport := pinport xor PB5;
+    LEDport := LEDport xor LEDpin;
     i := 0;
   end;
 end;
 
 begin
-  pindir := pindir or PB5;  // Set pin to output
+  countsPerSecond := (((F_CPU div 1024) + 128) div 256 div 3);  // 3 Hz
+  LEDdir := LEDdir or LEDpin;  // Set pin to output
+  LEDport := LEDport or LEDpin; // Set LED high
 
-  TCCR0A := %00000000;      // Normal mode
-  TCCR0B := %00000101;      // CS02, CS00 clock prescaler = 1024
-  TIMSK0 := %00000001;      // TOIE0 - trigger interrupt on timer overflow
-  asm sei end;              // Enable global interrupts
+  TCCR0A := 0;
+  TCCR0B := CS02msk or CS00msk;  // clock prescaler = 1024
+  // enable timer1 overflow interrupt
+  {$ifdef FPC_MCU_ATTINY45}
+  TIMSK
+  {$else} // defaults to atmega328p
+  TIMSK0
+  {$endif}
+  := TOIE0msk;
 
+  avr_sei;
   while True do;
 end.
 
