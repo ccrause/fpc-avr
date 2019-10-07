07256e19bce11e18c07ab1192e0a722bbae5bf90
ccrause <christo.crause@gmail.com>
2019-10-07 21:51:12 +0200

    Updated code to compile with new bit field format in RTL units

--- src/examples/blink4809/blink.pp
+++ src/examples/blink4809/blink.pp
@@ -3,11 +3,11 @@ program blink4809;
 uses delay, simavrinfo;
 
 begin
-  PORTF.DIRSET := 1 shl 5;
-  PORTF.OUT_ := 1 shl 5;
+  PORTF.DIRSET := Pin5bm;
+  PORTF.OUT_ := Pin5bm;
   while True do
   begin
-     PORTF.OUTTGL := 1 shl 5;
+     PORTF.OUTTGL := Pin5bm;
      delay_ms(100);    // delay 0.1 second
   end;
 end.
