ce1c609e00f02e317556a46d687f7827eda2fd4f
ccrause <christo.crause@gmail.com>
2022-02-13 16:22:08 +0200

    Add a simple Hello world example that outputs text via USART.

---
+++ src/examples/hello_world/hello_world.pas
@@ -0,0 +1,35 @@
+program hello_world;
+
+uses
+  consoleio, uart, delay, intrinsics;
+
+function WriteChar(ACh: char; AUserData: pointer): boolean;
+begin
+  WriteChar := true;
+  uart.uart_transmit(ord(Ach));
+end;
+
+function ReadChar(var ACh: char; AUserData: pointer): boolean;
+begin
+  ReadChar:=true;
+  ACh := char(uart.uart_receive);
+end;
+
+procedure initIO;
+begin
+  uart_init1(115200, true);
+  OpenIO(Input, @WriteChar, @ReadChar, fmInput, nil);
+  OpenIO(Output, @WriteChar, @ReadChar, fmOutput, nil);
+  OpenIO(ErrOutput, @WriteChar, @ReadChar, fmOutput, nil);
+  OpenIO(StdOut, @WriteChar, @ReadChar, fmOutput, nil);
+  OpenIO(StdErr, @WriteChar, @ReadChar, fmOutput, nil);
+end;
+
+begin
+  initIO;
+  repeat
+    writeln('Hello world');
+    delay_ms(1000);
+  until false;
+end.
+
