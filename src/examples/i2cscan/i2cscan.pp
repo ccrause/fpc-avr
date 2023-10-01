b72e28cc53761c2eb5b55ec74290036e718f648a
ccrause <christo.crause@gmail.com>
2023-10-01 20:48:27 +0200

    Replace writeString and writeHex with uart_transmit equivalents.

--- src/examples/i2cscan/i2cscan.pp
+++ src/examples/i2cscan/i2cscan.pp
@@ -3,23 +3,6 @@ program i2cscan;
 uses
   uart, i2c;
 
-const
-  HexChars: array[0..15] of char = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F');
-
-procedure writeString(s: shortstring);
-var
-  i: byte;
-begin
-  for i := 1 to length(s) do
-    uart.uart_transmit(ord(s[i]));
-end;
-
-procedure writeHex(b: byte);
-begin
-  uart.uart_transmit(ord(HexChars[b shr 4]));
-  uart.uart_transmit(ord(HexChars[b and 15]));
-end;
-
 var
   i2c_master: TI2CMaster;
 
@@ -39,26 +22,26 @@ var
   i, j, addr: byte;
 begin
   i2c_master.init(I2C_100kHz);
-  writeString('Starting scanning right adjusted addresses $00 - $7f.'#13#10);
-  writeString(#13#10);
-  writeString('   00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F'#13#10);
+  uart_transmit('Starting scanning right adjusted addresses $00 - $7f.'#13#10);
+  uart_transmit(#13#10);
+  uart_transmit('   00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F'#13#10);
   for i := 0 to 7 do
   begin
-    writeHex(i);
-    writeString('0 ');
+    uart_transmit_hex(i);
+    uart_transmit('0 ');
     for j := 0 to 15 do
     begin
       addr := (i shl 4) or j;
       if i2c_master.start(addr shl 1, true) then
       begin
-        writeHex(addr);
-        writeString(' ');
+        uart_transmit_hex(addr);
+        uart_transmit(' ');
       end
       else
-        writeString('.. ');
+        uart_transmit('.. ');
       i2c_master.stop;
     end;
-    writeString(#13#10);
+    uart_transmit(#13#10);
   end;
 end;
 
