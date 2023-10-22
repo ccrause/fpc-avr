33c3766eab7f955d1d989fe51a939a9c090d0593
ccrause <christo.crause@gmail.com>
2023-10-22 16:25:24 +0200

    Make more complete demo of DS1307 functionality over I2C.

--- src/examples/i2c/i2ctest.pp
+++ src/examples/i2c/i2ctest.pp
@@ -1,200 +1,185 @@
 program i2ctest;
 
 uses
-  uart, i2c;
-
-procedure writeString(s: string);
-var
-  i: byte;
-begin
-  for i := 1 to length(s) do
-    uart.uart_transmit(ord(s[i]));
-end;
-
-procedure writeHex(b: byte);
-begin
-  uart.uart_transmit((b shr 4) + ord('0'));
-  uart.uart_transmit((b and 7) + ord('0'));
-end;
+  uart, i2c, delay, simplemath;
 
 var
   i2c_master: TI2CMaster;
-  x: byte;
   c: char;
 
 const
   DS1307 = $68 shl 1;
-  BMPxxx = $77 shl 1;  // Address of BMP085/180/280* pressure sensor (* if SDO is high)
   BAUD_Rate = 115200;
+  // For x2 mode
   {$ifdef CPUAVRXMEGA3}
-  BAUD_SETTING = (F_CPU * 64 + 8 *BAUD_Rate) div (16 * BAUD_Rate);
+  BAUD_SETTING = (F_CPU*64 + 4*BAUD_Rate) div (8*BAUD_Rate);
   {$else}
-  BAUD_SETTING = ((F_CPU + 4*BAUD_Rate) shr 3) div BAUD_Rate;
+  BAUD_SETTING = (((F_CPU + 4*BAUD_Rate) shr 3) div BAUD_Rate) - 1;
   {$endif}
 
-  sOK = 'OK'#10;
-  sErr = 'ERROR'#10;
+  EOL = #13#10;
+  sErr = 'ERROR'+EOL;
 
 // Start clock if high bit of seconds is set
-function maybeStartDS1307Clock(): boolean;
+function setDS1307ClockState(const running: boolean): boolean;
 var
   s: byte;
 begin
   result := false;
-  i2c_master.init(I2C_100kHz);
+  if not i2c_master.ReadByteFromReg(DS1307, 0, s) then exit;
 
-  if not i2c_master.start(DS1307, I2C_WriteMode) then exit;
-  // set register address pointer to 0
-  if not i2c_master.writeByte(0) then exit;
-  if not i2c_master.start(DS1307, I2C_ReadMode) then exit;
-  if not i2c_master.readByte(s, false) then exit;
-
-  // High bit of seconds indicate if oscillator is halted (=1)
-  if (s and $80) = $80 then
+  if running then
   begin
-    // Clear high bit
-    s := s and $7F;
-    if not i2c_master.start(DS1307, I2C_WriteMode) then exit;
-    if not i2c_master.writeByte(0) then exit;
-    if not i2c_master.writeByte(s) then exit;
+    if (s and $80) = $80 then
+    begin
+      // Clear high bit
+      s := s and $7F;
+      i2c_master.WriteByteToReg(DS1307, 0, s);
+    end;
+  end
+  else
+  begin
+    if (s and $80) = 0 then
+    begin
+      // Set high bit
+      s := s or $80;
+      if not i2c_master.WriteByteToReg(DS1307, 0, s) then exit;
+    end;
   end;
 
-  i2c_master.stop;
   result := true;
 end;
 
-// Stop clock by setting high bit of seconds to 1
-function haltDS1307Clock(): boolean;
+// Use compiled date & time to initialize RTC
+// Also enable clock
+function initializeDateTime: boolean;
 var
-  s: byte;
+  t: array[0..6] of byte;
+  d, r: byte;
+begin
+  // Numbers are stored as BCD, so two digits per byte
+  d := {$include %timesecond%};
+  divmod10(d, r);
+  t[0] := (d shl 4) or r; // autmatically start clock running
+  d := {$include %timeminute%};
+  divmod10(d, r);
+  t[1] := (d shl 4) or r;
+  d := {$include %timehour%};
+  divmod10(d, r);
+  t[2] := (d shl 4) or r; // automatically set hour mode to 24H
+
+  // Day of week
+  t[3] := 0;
+
+  d := {$include %dateday%};
+  divmod10(d, r);
+  t[4] := (d shl 4) or r;
+  d := {$include %datemonth%};
+  divmod10(d, r);
+  t[5] := (d shl 4) or r;
+  d := ({$include %dateyear%} mod 100);
+  divmod10(d, r);
+  t[6] := (d shl 4) or r;
+
+  Result := i2c_master.WriteBytesToReg(DS1307, 0, @t[0], length(t));
+end;
+
+function printDS1307DateTime(): boolean;
+const
+  HexChars: array[0..15] of char = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F');
+var
+  t: array[0..6] of byte;
 begin
   result := false;
-  i2c_master.init(I2C_100kHz);
-
-  if not i2c_master.start(DS1307, I2C_WriteMode) then exit;
-  // set register address pointer to 0
-  if not i2c_master.writeByte(0) then exit;
-  if not i2c_master.start(DS1307, I2C_ReadMode) then exit;
-  if not i2c_master.readByte(s, false) then exit;
-
-  // High bit of seconds indicate if oscillator is halted (=1)
-  if (s and $80) = 0 then
-  begin
-    // Clear high bit
-    s := s or $80;
-    if not i2c_master.start(DS1307, I2C_WriteMode) then exit;
-    if not i2c_master.writeByte(0) then exit;
-    if not i2c_master.writeByte(s) then exit;
-  end;
-
-  i2c_master.stop;
+  if not i2c_master.ReadBytesFromReg(DS1307, 0, @t[0], length(t)) then exit;
   result := true;
-end;
 
-function printDS1307Time(): boolean;
-var
-  s, m, h: byte;
-begin
-  result := false;
-  i2c_master.init(I2C_100kHz);
-  if not i2c_master.start(DS1307, I2C_WriteMode) then exit;
-  // set register to # 0
-  if not i2c_master.writeByte(0) then exit;
-  if not i2c_master.start(DS1307, I2C_ReadMode) then exit;
-  if not i2c_master.readByte(s, true) then exit;
-  if not i2c_master.readByte(m, true) then exit;
-  if not i2c_master.readByte(h, false) then exit;
-  i2c_master.stop;
-
-  result := true;
-  uart.uart_transmit((h shr 4) and $03 + ord('0'));
-  uart.uart_transmit(h and $0F + ord('0'));
+  // Print years
+  uart.uart_transmit(ord(HexChars[t[6] shr 4]));
+  uart.uart_transmit(ord(HexChars[t[6] and $0F]));
+  uart.uart_transmit(ord('/'));
+  // Print months
+  uart.uart_transmit(ord(HexChars[t[5] shr 4]));
+  uart.uart_transmit(ord(HexChars[t[5] and $0F]));
+  uart.uart_transmit(ord('/'));
+  // Print days
+  uart.uart_transmit(ord(HexChars[t[4] shr 4]));
+  uart.uart_transmit(ord(HexChars[t[4] and $0F]));
+  uart.uart_transmit(' ');
+  // Skip day of week
+  // Print hours
+  uart.uart_transmit(ord(HexChars[(t[2] shr 4) and $03]));
+  uart.uart_transmit(ord(HexChars[t[2] and $0F]));
   uart.uart_transmit(ord(':'));
 
-  uart.uart_transmit((m shr 4) and $07 + ord('0'));
-  uart.uart_transmit(m and $0F + ord('0'));
+  // Print minutes
+  uart.uart_transmit(ord(HexChars[(t[1] shr 4) and $07]));
+  uart.uart_transmit(ord(HexChars[t[1] and $0F]));
   uart.uart_transmit(ord(':'));
 
-  uart.uart_transmit((s shr 4) and $07 + ord('0'));
-  uart.uart_transmit(s and $0F + ord('0'));
-  uart.uart_transmit(10);
+  // Print seconds
+  uart.uart_transmit(ord(HexChars[(t[0] shr 4) and $07]));
+  uart.uart_transmit(ord(HexChars[t[0] and $0F]));
+  uart.uart_transmit(EOL);
 end;
-
 
 begin
   uart.uart_init(BAUD_SETTING);
+  {$ifdef CPUAVRXMEGA3}
+  // Configure RX & TX pins for USART3 of atmega48809
+  PORTB.DIRCLR := PIN1bm; // RX in input mode
+  PORTB.DIRSET := Pin0bm; // TX in output mode
+  {$endif}
+
+  uart_transmit('I2C init'+EOL);
+  i2c_master.init(I2C_100kHz);
+
+  uart_transmit('Interactive commands:'+EOL);
+  uart_transmit('  h - Halt RTC timer'+EOL);
+  uart_transmit('  i - Initialize RTC timer'+EOL);
+  uart_transmit('  r - Run RTC timer'+EOL);
 
   repeat
-    i2c_master.init(I2C_100kHz);
-    maybeStartDS1307Clock();
+    delay_ms(2000);
+    if not printDS1307DateTime() then
+    begin
+      uart_transmit(sErr);
+      uart_Transmit('printDS1307Time'+EOL);
+    end;
 
-    repeat
-      writestring('Enter char to proceed [R] to restart'#10);
-      c := char(uart_receive());
-
+    if uart_peek(byte(c)) then
+    begin
       if c = 'h' then
       begin
-        writeString('HALT'#10);
-        haltDS1307Clock()
+        uart_transmit('HALT');
+        if setDS1307ClockState(false) then
+          uart_transmit(EOL)
+        else
+          uart_transmit(sErr);
+      end
+      else if c = 'i' then
+      begin
+        uart_transmit('INIT');
+        if initializeDateTime then
+          uart_transmit(EOL)
+        else
+          uart_transmit(sErr);
       end
       else if c = 'r' then
       begin
-        writeString('START'#10);
-        maybeStartDS1307Clock()
+        uart_transmit('START');
+        if setDS1307ClockState(true) then
+          uart_transmit(EOL)
+        else
+          uart_transmit(sErr);
       end
       else
       begin
-        if not printDS1307Time() then
-        writeString(sErr);
+        uart_transmit('Ignoring character: ');
+        uart_transmit(c);
+        uart_transmit(EOL);
       end;
-    until c = 'R'; // restart i2c
+    end;
   until false;
-
-// read BMPxxx ID
-{  repeat
-    writeString('Init I2C @ 100 kHz'#10);
-    i2c_master.init(I2C_100kHz);
-
-    writeString('Start: ');
-    if i2c_master.start(BMPxxx, false) then // start in write mode
-      writeString(sOK)
-    else
-      writeString(sErr);
-
-    uart_receive();
-    writeString('writeByte: ');
-    if i2c_master.writeByte($D0) then // set register to ID register
-      writeString(sOK)
-    else
-      writeString(sErr);
-
-    uart_receive();
-    writeString('Start: ');
-    if i2c_master.start(BMPxxx, true) then // start in read mode
-      writeString(sOK)
-    else
-      writeString(sErr);
-
-    uart_receive();
-    writeString('readByte: ');
-    if i2c_master.readByte(x, false) then    // read ID
-      writeString(sOK)
-    else
-      writeString(sErr);
-
-    uart_receive();
-    writeString('Stop');
-    i2c_master.stop;
-
-    // Write ID
-    writeString('BMP ID: $');
-    writeHex(x);
-    uart_transmit(10);
-
-    writeString(#10#10'[R]estart...'#10);
-    repeat
-    until uart.uart_receive() = ord('R');
-  until false;
-}
 end.
 
