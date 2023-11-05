eda43f240c02035d24750400b00aa315f82caa66
ccrause <christo.crause@gmail.com>
2023-11-05 10:48:56 +0200

    Add debug serial output. Add reboot option. Data received now copied to counter.

--- src/examples/i2cslave/i2cslavedemo.pp
+++ src/examples/i2cslave/i2cslavedemo.pp
@@ -1,10 +1,11 @@
 program i2cslavedemo;
 
-{
-  This demo implements a simple I2C slave device.
+{ This demo implements a simple I2C slave device.
   On I2C read, a byte sized counter value is returned and the counter is incremented.
-  On I2C write, the received data byte is transmitted over UART as a hex string.
-}
+  On I2C write, the received data byte is copied to the counter value.
+  If DEBUGPRINT is defined, some diagnostics are printed. }
+
+{$define DEBUGPRINT}
 
 uses
   uart, i2cslave_unit, delay;
@@ -20,32 +21,45 @@ const
   DeviceAddress = $18 shl 1; // Left adjusted byte address, no response to general call address.
 
 var
-  counter: byte;
+  counter, c: byte;
 
-// Echo each received byte as hex to uart
+// Set current counter value to received byte
 procedure DataReceivedHandler(data: byte; ACK: boolean);
 begin
-  uart_transmit(ord('$'));
+  {$ifdef DEBUGPRINT}
+  uart_transmit(ord('R'));
   uart_transmit_hex(data);
-  if not ACK then
-    //uart_transmit('A');
+  if ACK then
+    uart_transmit('ack')
+  else
+    uart_transmit('nack');
   uart_transmit(10);
+  {$endif DEBUGPRINT}
+
+  counter := data;
 end;
 
-// Reply to a read request with a single '!' char
+// Reply with current counter value, then increment counter
 procedure DataRequestHandler(out data: byte; out ACK: boolean);
 begin
   data := counter;
-  uart_transmit('Counter: ');
-  uart_transmit_asstring(counter);
-  uart_transmit(10);
-  ACK := false;
   inc(counter);
+  // Can supply more data
+  ACK := true;
+
+  {$ifdef DEBUGPRINT}
+  uart_transmit(ord('T'));
+  uart_transmit_hex(data);
+  uart_transmit(10);
+  {$endif DEBUGPRINT}
 end;
 
 procedure ResetHandler;
 begin
+  // Nothing to do for this example
+  {$ifdef DEBUGPRINT}
   uart_transmit('Reset'#10);
+  {$endif DEBUGPRINT}
 end;
 
 begin
@@ -59,16 +73,36 @@ begin
   uart_transmit('Starting I2C slave @ $');
   uart_transmit_hex(byte(DeviceAddress shr 1));  //
   uart_transmit(#10);
+  uart_transmit('To reboot controller send a "r" character over serial.'#10#10);
+  {$ifdef DEBUGPRINT}
+  uart_transmit('Debug message formats:'#10);
+  uart_transmit('  R - received byte as hex, followed by ack state'#10);
+  uart_transmit('  T - transmitted byte as hex.'#10);
+  uart_transmit('  Reset - Reset handler called.'#10);
+  {$endif DEBUGPRINT}
 
   counter := 0;
-  // No reset callback since no state is preserved in implementation
-  i2cslave_listen(DeviceAddress, @DataReceivedHandler,
-                  @DataRequestHandler, @ResetHandler);  // Do not respond to general call address
+  i2cslave_listen(DeviceAddress, @DataReceivedHandler, @DataRequestHandler,
+                  @ResetHandler);
 
-  // Endless loop transmitting a . over serial as heartbeat indicator
   repeat
     delay_ms(1000);
-    uart_transmit('.');
+    if uart_peek(c) and (c = ord('r')) then
+    begin
+      uart_transmit(#10'Rebooting'#10);
+      {$if declared(CPU)}
+      delay_ms(10);
+      CPU.CCP := $D8;
+      RSTCTRL.SWRR := 1;
+      {$elseif declared(WDTCSR)}
+      // Enable change bit
+      WDTCSR := 1 shl WDCE;
+      // System reset mode, 16 ms timeout
+      WDTCSR := (1 shl WDE);
+      {$else}
+        {$error Unknown WDT control register}
+      {$endif}
+    end;
   until false;
 end.
 
