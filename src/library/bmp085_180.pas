b5bdef1c9a93d96712887021c32f26e2e64e3152
ccrause <christo.crause@gmail.com>
2025-12-17 13:32:22 +0200

    Use custom integer division to reduce code size

--- src/library/bmp085_180.pas
+++ src/library/bmp085_180.pas
@@ -2,6 +2,15 @@ unit bmp085_180;
 
 // This unit interfaces with the Bosch BMP085 & BMP180 sensors over I2C.
 
+{ This is a math intensive unit that calls custom division routines that
+  do not check for divide by zero and omits the associated error handling
+  overhead. This saves about 570 bytes of code.
+
+  To revert to the standard compiler division routines activate the
+  OriginalCode define below                                                  }
+
+{ $define OriginalCode}
+
 interface
 
 // Initialize I2C and load calibration constants
@@ -12,7 +21,7 @@ function bmp_init: boolean;
 // and pressure in Pascal
 // This function sets the oversampling parameter to its lowest value (oss = 0)
 // to simplify the code a little bit.
-function readTP(out T, P: int32): boolean;
+function readTP(out T: int32; out P: uint32): boolean;
 
 // Reads ID of sensor ($55 for BMP085/180)
 // Returns false on error
@@ -21,7 +30,7 @@ function readID(out ID: byte): boolean;
 implementation
 
 uses
-  i2c, delay;
+  i2c, delay, integermath;
 
 type
   TCalibrationConsts = record
@@ -62,7 +71,7 @@ end;
 // and pressure in Pascal
 // This function sets the oversampling parameter to its lowest value (oss = 0)
 // to simplify the code a little bit.
-function readTP(out T, P: int32): boolean;
+function readTP(out T: int32; out P: uint32): boolean;
 var
   UT, UP: int32;
   h, l, xl: byte;
@@ -79,10 +88,14 @@ begin
   if not i2c_master.ReadByteFromReg(BMPxxx, $F7, l) then exit;
   UT := int32((word(h) shl 8) or l);
 
-  X1 := (int32(UT - consts.AC6)*consts.AC5) div (1024*2*16);
-  X2 := (int32(consts.MC) shl 11) div (X1 + int32(consts.MD));
+  {$ifdef OriginalCode} X1 := (int32(UT - consts.AC6)*consts.AC5) shr 15; {$else} //div (1024*2*16);
+  div_mod_int32(int32(UT - consts.AC6)*consts.AC5, (1024*2*16), X1, X2); {$endif}
+
+  {$ifdef OriginalCode} X2 := (int32(consts.MC) shl 11) div (X1 + int32(consts.MD)); {$else}
+  div_mod_int32(int32(consts.MC) shl 11, X1 + int32(consts.MD), X2, B5);{$endif}
   B5 := X1 + X2;
-  T := int32(B5 + 8) div 16;
+  {$ifdef OriginalCode} T := int32(B5 + 8) shr 4; {$else} //div 16;
+  div_mod_int32(int32(B5 + 8), 16, T, X1); {$endif}
 
   // Request pressure measurement - oss = 0 (lowest resolution)
   if not i2c_master.WriteByteToReg(BMPxxx, $F4, $34) then exit;
@@ -93,26 +106,55 @@ begin
   UP := (word(h) shl 8) or l;
 
   B6 := B5 - 4000;
-  X1 := (consts.B2 * ((B6*B6) shr 12)) div 2048;
-  X2 := int32(consts.AC2 * B6) div 2048;
-  X3 := X1 + X2;
-  B3 := ((int32(consts.AC1)*4 + X3) + 2) div 4;  // NB: Excludes shl oss
 
-  X1 := int32(consts.AC3*B6) div (8*1024);
-  X2 := (consts.B1 * ((B6*B6) shr 12)) div (256*256);
-  X3 := (X1 + X2 + 2) div 4; //shr 2;
+  {$ifdef OriginalCode} X1 := (consts.B2 * ((B6*B6) shr 12)) shr 11;{$else} //div 2048;
+  div_mod_int32(consts.B2 * ((B6*B6) shr 12),  2048, X1, X2); {$endif}
+
+  {$ifdef OriginalCode} X2 := int32(consts.AC2 * B6) shr 11; {$else}//div 2048;
+  div_mod_int32(int32(consts.AC2 * B6), 2048, X2, X3);{$endif}
+
+  X3 := X1 + X2;
+
+  {$ifdef OriginalCode}B3 := ((int32(consts.AC1)*4 + X3) + 2) shr 2; {$else}//div 4;  // NB: Excludes shl oss
+  div_mod_int32((int32(consts.AC1)*4 + X3) + 2, 4, B3, X1); {$endif} // NB: Excludes shl oss
+
+  {$ifdef OriginalCode} X1 := int32(consts.AC3*B6) shr 13; {$else}//div (8*1024);
+  div_mod_int32(int32(consts.AC3*B6), (8*1024), X1, X2);{$endif}
+
+  {$ifdef OriginalCode}X2 := (consts.B1 * ((B6*B6) shr 12)) shr 16;{$else} //div (256*256);
+  div_mod_int32(consts.B1 * ((B6*B6) shr 12), (256*256), X2, X3);{$endif}
+
+  {$ifdef OriginalCode}X3 := (X1 + X2 + 2) shr 2;{$else} //div 4; //shr 2;
+  div_mod_int32(X1 + X2 + 2, 4, X3, B5);{$endif}
+
   B4 := (consts.AC4 * (uint32(X3) + 32768)) shr 15;
+
   B7 := (int32(UP) - B3) * 50000 {shr oss};
 
   if B7 < $80000000 then
-    P := (B7*2) div B4
+  begin
+    {$ifdef OriginalCode}P := (B7*2) div B4;{$else}
+    div_mod_dword(B7*2, B4, P, dword(UP));{$endif}
+  end
   else
-    P := (B7 div B4) * 2;
+  begin
+    {$ifdef OriginalCode}P := (B7 div B4) * 2;{$else}
+    div_mod_dword(B7, B4, dword(P), dword(UP));
+    P := P*2;{$endif}
+  end;
 
   X1 := (P shr 8) * (P shr 8);
-  X1 := (X1*3038) div (256*256);
-  X2 := (-7357 * P) div (256*256);
-  P := P + (X1 + X2 + 3791) div 16;
+
+  {$ifdef OriginalCode}X1 := (X1*3038) shr 16; {$else}//div (256*256);
+  div_mod_int32(X1*3038, 256*256, X1, X2);{$endif}
+
+  {$ifdef OriginalCode}X2 := (-7357 * P) shr 16;{$else} //div (256*256);
+  div_mod_int32(-7357 * P, 256*256, X2, B5);{$endif}
+
+  {$ifdef OriginalCode}P := P + (X1 + X2 + 3791) shr 4;{$else} //div 16;
+  div_mod_int32(X1 + X2 + 3791, 16, X3, B5);
+  P := P + X3;{$endif}
+
   Result := true;
 end;
 
